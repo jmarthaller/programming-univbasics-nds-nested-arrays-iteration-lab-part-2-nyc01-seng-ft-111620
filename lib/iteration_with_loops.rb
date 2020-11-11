a3d2b7b7ef4a2be39ef9f6ff3ee7591f25ef309c
Jacob Marthaller <marthaller.jr@gmail.com>
2020-11-11 19:50:15 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,35 +1,64 @@
+
+require 'pry'
+
+# src will be an array of arrays of integers
+# Produce a new Array that contains the smallest number of each of the nested arrays
+
 # def find_min_in_nested_arrays(src)
-#   # src will be an array of arrays of integers
-#   # Produce a new Array that contains the smallest number of each of the nested arrays
 #   result_array = []
 #   row_index = 0
-#   while row_index < src.count do
+  
+#   while row_index < src.length do
+
 #     element_index = 0
-    
-#       while element_index < src[row_index].count do
-#         #smallest_value = 0
-#         # if src[row_index][element_index > smallest_value
-#         #   smallest_value = src[row_index][element_index]
-#         #   result_array.push(smallest_value)
-#         # end
-#         result_array.push(src[row_index].min)
+#       while element_index < src[row_index].length do
+#         #currently smallest_value is 59, which is + 1 from the biggest number in the [0] array
+#         smallest_value = src[row_index][element_index]
+#         if src[row_index][element_index] > smallest_value 
+#           binding.pry
+#           result_array.push(src[row_index][element_index])
+#         end
 #         element_index += 1
 #       end
+#       row_index += 1
+#     end
+#   result_array
+# end
+
+
+# # # Returns the correct array using a sort method
+# def find_min_in_nested_arrays(src)
+#   result_array = []
+#   row_index = 0
+#   while row_index < src.length do
+#     #element_index = 0
+#     smallest_value = src[row_index].sort[0]
+#     result_array.push(smallest_value)
 #       row_index += 1
 #     end
 #   return result_array
 # end
 
 
-# Returns the correct array using a sort method
+
+
+
 def find_min_in_nested_arrays(src)
   result_array = []
   row_index = 0
   while row_index < src.length do
-    #element_index = 0
-    smallest_value = src[row_index].sort[0]
-    result_array.push(smallest_value)
-      row_index += 1
+    element_index = 0
+    smallest_value = src[row_index][element_index]
+    while element_index < src[row_index].length do 
+      if smallest_value > src[row_index][element_index]
+        smallest_value = src[row_index][element_index]
+      end
+      element_index += 1
     end
-  return result_array
+    result_array.push(smallest_value)
+    row_index += 1
+  end
+  result_array
 end
+
+ 
